2ac0867459080e2bdecba3652f3ea54850411341
Leif-W <leif.westerlind@gmail.com>
2020-06-20 14:17:27 -0400

    Check for non hex chars in NEWS_API_KEY

--- util/checkEnv.js
+++ util/checkEnv.js
@@ -2,6 +2,7 @@
 
 const fs = require( 'fs' );
 const path = '.env';
+const regexp = /^[a-f0-9]{32}$/;
 
 if ( fs.existsSync( path ) ) {
     console.log( `Found ${ path }` );
@@ -10,7 +11,10 @@ if ( fs.existsSync( path ) ) {
     console.error( 'Error: No .env file and no NEWS_API_KEY variable in process.env' );
     return 1;
 } else if ( process.env.NEWS_API_KEY.length < 32 ) {
-    console.error( `Error: Invalid NEWS_API_KEY ${ process.env.NEWS_API_KEY }, too short.` );
+    console.error( `Error: Invalid NEWS_API_KEY, too short.` );
+    return 1;
+} else if ( ! regexp.test( $process.env.NEWS_API_KEY ) ) {
+    console.error( `Error: NEWS_API_KEY contains invalid characters.` );
     return 1;
 } else {
     fs.writeFileSync( path, `NEWS_API_KEY=${ process.env.NEWS_API_KEY }\n` );
